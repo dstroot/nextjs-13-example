997d0065a6c677a12c72fe9bcb41b8b371a41d4a
Dan Stroot <dan@thestroots.com>
2023-01-04 12:26:46 -0800

    menu spacing

---
+++ app/error.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+// import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  // // Log the error to an error reporting service
+  // useEffect(() => {
+  //   console.error(error);
+  // }, [error]);
+
+  return (
+    <div>
+      <p>{error.message}</p>
+      <button onClick={() => reset()}>Reset error boundary</button>
+    </div>
+  );
+}
