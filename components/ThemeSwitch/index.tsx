419bc9bdbf4d7993224dddaf4e8a1448eb37d1a4
Dan Stroot <dan@thestroots.com>
2023-01-24 20:19:24 -0800

    updated dependencies

--- components/ThemeSwitch/index.tsx
+++ components/ThemeSwitch/index.tsx
@@ -4,13 +4,13 @@ import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 
 // icons
-import { FiSun } from "react-icons/fi";
-import { FiMoon } from "react-icons/fi";
-import { FiSettings } from "react-icons/fi";
+// import { FiSun } from "react-icons/fi";
+// import { FiMoon } from "react-icons/fi";
+// import { FiSettings } from "react-icons/fi";
 
-// import { BsSun } from "react-icons/bs";
-// import { BsMoonStars } from "react-icons/bs";
-// import { GrSystem } from "react-icons/gr";
+import { BsSun } from "react-icons/bs";
+import { BsMoonStars } from "react-icons/bs";
+import { BsGear } from "react-icons/bs";
 
 // library
 import { cn } from "../../lib/cn";
@@ -30,22 +30,22 @@ export const ThemeSwitch = () => {
 
   return (
     <>
-      <div className="flex justify-between mx-auto bg-white border-2 border-gray-500 rounded-full dark:bg-gray-300 dark:border-gray-300 w-28">
+      <div className="flex justify-between w-20 mx-auto bg-white border-2 border-gray-500 rounded-full dark:bg-gray-300 dark:border-gray-300">
         <button
           id="light"
           className={cn(
-            "hover:transition-all w-8 h-8 p-2 rounded-full hover:bg-gray-500 hover:text-white",
+            "hover:transition-all w-6 h-6 p-1 rounded-full hover:bg-gray-500 hover:text-white",
             theme === "light" ? "bg-gray-500 text-white" : "text-black"
           )}
           onClick={(e) => setTheme("light")}
         >
-          <FiSun />
-          {/* <BsSun /> */}
+          {/* <FiSun /> */}
+          <BsSun />
         </button>
         <button
           id="system"
           className={cn(
-            "hover:transition-all w-8 h-8 p-2 rounded-full hover:bg-gray-500 hover:text-white",
+            "hover:transition-all w-6 h-6 p-1 rounded-full hover:bg-gray-500 hover:text-white",
             theme === "system"
               ? "text-white bg-gray-500 dark:text-white"
               : "text-black"
@@ -53,19 +53,19 @@ export const ThemeSwitch = () => {
           aria-label="Toggle Light/Dark Mode - System Setting"
           onClick={(e) => setTheme("system")}
         >
-          <FiSettings />
-          {/* <GrSystem /> */}
+          {/* <FiSettings /> */}
+          <BsGear />
         </button>
         <button
           id="dark"
           className={cn(
-            "hover:transition-all w-8 h-8 p-2 rounded-full hover:bg-gray-500 dark:hover:text-white hover:text-white",
+            "hover:transition-all w-6 h-6 p-1 rounded-full hover:bg-gray-500 dark:hover:text-white hover:text-white",
             theme === "dark" ? "bg-gray-500 text-white" : "dark:text-black"
           )}
           onClick={(e) => setTheme("dark")}
         >
-          <FiMoon />
-          {/* <BsMoonStars /> */}
+          {/* <FiMoon /> */}
+          <BsMoonStars />
         </button>
       </div>
     </>
