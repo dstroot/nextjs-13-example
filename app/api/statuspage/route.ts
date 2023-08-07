26ed63a1012e3aa40bdb9f551a4b711c988fce5e
Dan Stroot <dan.stroot@gmail.com>
2023-08-07 11:09:44 -0700

    updated dependencies

--- app/api/statuspage/route.ts
+++ app/api/statuspage/route.ts
@@ -1,8 +1,24 @@
 import { NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
+import { Redis } from '@upstash/redis';
+import { Ratelimit } from '@upstash/ratelimit';
 
 export const runtime = 'edge';
 
-export async function GET() {
+const ratelimiter = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(5, '10 s'),
+  analytics: true,
+});
+
+export async function GET(req: NextRequest) {
+  // rate limit requests
+  const ip = req.headers.get('x-forwarded-for');
+  const { success } = await ratelimiter.limit(ip || 'api');
+  if (!success) {
+    return new NextResponse('Rate limit exceeded', { status: 429 });
+  }
+
   const res = await fetch('https://9f5hpdsv6r8j.statuspage.io/api/v2/summary.json', {
     next: { revalidate: 60 }, // Revalidate every 60 seconds
     // headers: {
--- lib/cn.ts
+++ lib/cn.ts
@@ -1,15 +1,20 @@
-export function cn(...classes: string[]) {
-  return classes.filter(Boolean).join(' ');
+// https://www.youtube.com/watch?v=re2JFITR7TI
+
+import { twMerge } from 'tailwind-merge';
+import { clsx, ClassValue } from 'clsx';
+
+export function cn(...classes: ClassValue[]) {
+  return twMerge(clsx(classes));
 }
 
 /*
 usage:
 
-import { cn } from "./cn";
+import { cn } from "@/lib/cn";
 
 export const Navbar = ({ fixed }: { fixed: boolean }) => {
   return (
-    <nav className={cn("bg-white", fixed ? "fixed top-0 left-0 right-0" : "")}>
+    <nav className={cn("bg-white", fixed ?? "fixed top-0 left-0 right-0")}>
       Item
     </nav>
   );
